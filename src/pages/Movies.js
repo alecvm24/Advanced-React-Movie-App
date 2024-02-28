77a69bdffd41f0447a9251fbe5ae0518006f7c26
Alec VM <alecvm24@hotmail.com>
2024-02-28 12:04:23 +0100

    Add useEffect, useState and Filtering

---
+++ src/pages/Movies.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieList from "./src/components/MovieList";
+import FilterOptions from "./src/components/FilterOptions";
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://www.omdbapi.com/")
+      .then((response) => {
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
+  });
+
+  const filteredMovies = filter
+    ? movies.filter((movie) =>
+        movie.title.toLowerCase().includes(filter.toLowerCase())
+      )
+    : movies;
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <FilterOptions setFilter={setFilter} />
+      <MovieList movies={filteredMovies} />
+    </div>
+  );
+
+  return <div>{/* Header, FilterOptions, and MovieList */}</div>;
+};
+
+export default Movies;
